3f1edaedb673774eba2a424cc827a3a55a71dc1b
W. Brian Gourlie <bgourlie@gmail.com>
2014-05-11 11:06:07 -0500

    Formatting.

--- lib/varxa_ui.dart
+++ lib/varxa_ui.dart
@@ -8,7 +8,6 @@ import 'package:logging/logging.dart';
 part 'src/varxa_button/varxa_button.dart';
 part 'src/varxa_button_group/varxa_button_group.dart';
 
-
 final _logger = new Logger('varxa_ui');
 
 class VarxaUiModule extends Module {
